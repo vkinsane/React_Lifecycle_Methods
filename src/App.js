c2f3fd6a7f06b7ae93bb4632448a3fe445d5036f
Vishal Khandate <vishukblog000@gmail.com>
2021-04-24 16:11:46 +0530

    Uncomment Update Phase 2

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import "./App.css";
-import MountingPhase from "./components/MountingPhase";
-// import UpdatingPhase from "./components/UpdatingPhase";
+// import MountingPhase from "./components/MountingPhase";
+import UpdatingPhase from "./components/UpdatingPhase";
 // import UnmountingPhase from "./components/UnmountingPhase";
 
 class App extends Component {
@@ -29,14 +29,14 @@ class App extends Component {
         {/* <MountingPhase /> */}
 
         {/* ---------------Updating Phase--------------- */}
-        {/* <div className="update-phase">
+        <div className="update-phase">
           <h1>Parent Component</h1>
           <div style={{ fontSize: "5em" }}>{this.state.device}</div>
           <button className="btn btn-outline-dark" onClick={this.changeDevice}>
             Change Props
           </button>
           <UpdatingPhase device={this.state.device} />
-        </div> */}
+        </div>
         {/* ---------------Unmounting Phase--------------- */}
         {/* <UnmountingPhase /> */}
       </div>
